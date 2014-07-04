f785ad4b61e2f7e4bfe46fb062ab36767fc0f557
Giannis Dzegoutanis <erasmospunk@gmail.com>
2014-07-04 13:20:52 +0200

    init

---
+++ update.sh
@@ -0,0 +1,15 @@
+#git commit -a --amend
+
+rm -rf _site
+jekyll build
+git checkout gh-pages
+#rm -rf img/*
+mv _site/img/* img/
+mv _site/css/main.css css/
+mv _site/index.html ./
+mv _site/favicon.ico ./
+rm -rf _site
+
+git commit -am "update"
+git push
+git checkout master
